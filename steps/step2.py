288f39c1298a17d1eb1c4b80fb3f729017b563d0
Flávio Oliveira <73088855+flaviojoliveira@users.noreply.github.com>
2020-11-24 15:30:00 -0300

    update step2

--- steps/step2.py
+++ steps/step2.py
@@ -1,8 +1,24 @@
 import speech_recognition as sr
+import pyttsx3
+from random import choice
 
+#No step anterior nosso código estava com apenas uma resposta no return#
+#criamos "feedback_fala" com uma lista de respostas para os casos de erro#
 
-def reconhece():
-	rec = sr.Recognizer()
+erros_feedback = [
+		"Desculpe, não entendi",
+		"Pode repetir por favor",
+		"Que língua é esta?"
+]
+
+fala = pyttsx3.init()
+
+def execucao(retorno)
+   		fala.say.(retorno)
+   		fala.runAndWait()
+
+def identifica(feedback_fala):
+		rec = sr.Recognizer()
 
 	with sr.Microphone() as s:
 		rec.adjust_for_ambient_noise(s)
@@ -13,8 +29,12 @@ def reconhece():
 				entrada = rec.recognize_google(audio, language="pt")
 				return "Você disse: {}".format(entrada)
 			except sr.UnknownValueError:
-				return "Não entendi nada"
-print("Ouvindo...\n-----------------\n")
+				return feedback_fala
+
+print("Estou lhe ouvindo... \n**********\n")
+
 while True:
-	fala = reconhece()
-	print(fala)
+	feedback_fala = choice(erros_feedback)
+	voz = identifica(feedback_fala)
+	print("Você disse: {}".format(voz))
+	execucao(voz)
